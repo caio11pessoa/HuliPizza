a375117c171842acb61b5bab395f2ba41d66a8e6
CaioPessoa <caiopessoa1298@gmail.com>
2025-05-21 17:13:33 -0300

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Caio de Almeida Pessoa on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
